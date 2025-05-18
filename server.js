97073b9b6d7b95d7e9fffe4b3a086d64601ad0d7
Kadir70-dev <kadirab1999@gmail.com>
2025-05-19 03:58:47 +0530

    get api

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => { res.send('API is running...')} );
+
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 
